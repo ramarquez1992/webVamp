57c6499e94c03bab2a3e5d142386db15d23b771e
Richard Marquez <richard92m@me.com>
2017-04-01 16:11:45 -0500

    refactored effect controls

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
 var audioContext = getAudioCtx();
-var BUFF_SIZE = 16384;
-var audioInput = null,
-  microphoneStream = null,
-  gainNode = null,
-  scriptProcessorNode = null,
-  scriptProcessorFFTNode = null,
-  analyserNode = null;
+var volumeNode,
+  gainNode,
+  toneNode,
+  reverbNode;
 
 $(document).ready(function () {
-  initAudioInput();
   initGUI();
+  initAudioInput();
 });
 
 function initGUI() {
-  //alert(asdf);
+  document.getElementById('volume').addEventListener('change', function () {
+    volumeNode.gain.value = this.value;
+  });
+
+  document.getElementById('gain').addEventListener('change', function () {
+    gainNode.gain.value = this.value;
+  });
+
+  document.getElementById('tone').addEventListener('change', function () {
+    toneNode.frequency.value = this.value;
+  });
+
+  document.getElementById('reverb').addEventListener('change', function () {
+    reverbNode.time = this.value;
+  });
 }
 
 function initAudioInput() {
-  console.log("audio is starting up ...");
-
+  // Assign navigator.getUserMedia on per browser basis
   if (!navigator.getUserMedia)
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
       navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
+  // Start stream if possible, else display error
   if (navigator.getUserMedia) {
-
     navigator.getUserMedia({audio: true},
       function (stream) {
         startMicrophone(stream);
@@ -39,67 +49,30 @@ function initAudioInput() {
   }
 }
 
-
-// ---
-
-
-function getAudioCtx() {
-  if ( window.AudioContext ) return new window.AudioContext();
-  else if ( window.webkitAudioContext ) return new window.webkitAudioContext();
-  else if ( window.audioContext ) return new window.audioContext();
-  else alert( 'Browser audio requirements not met' );
-}
-
-function processMicrophoneBuffer(event) {
-  var microphoneOutputBuffer = event.inputBuffer.getChannelData(0); // contains current frame of data size BUFF_SIZE
-  //draw( microphoneOutputBuffer, 0, 'freqDomainCanvas' );
-}
-
 function startMicrophone(stream) {
-  microphoneStream = audioContext.createMediaStreamSource(stream);
+  var microphoneStream = audioContext.createMediaStreamSource(stream);
 
-  // VOLUME
+  // Volume
   volumeNode = audioContext.createGain();
 
-  document.getElementById('volume').addEventListener('change', function () {
-    var currVolume = this.value;
-    volumeNode.gain.value = currVolume;
-  });
-
-  // GAIN
+  // Gain
   gainNode = audioContext.createGain();
 
-  document.getElementById('gain').addEventListener('change', function () {
-    var currGain = this.value;
-    gainNode.gain.value = currGain;
-  });
+  // Tone
+  toneNode = audioContext.createBiquadFilter();
+  toneNode.type = "highshelf";
+  toneNode.frequency.value = 2000;  // Hz
+  toneNode.gain.value = 50;
 
-
-  // REVERB
-  var reverbNode = Reverb(audioContext);
+  // Reverb
+  reverbNode = Reverb(audioContext);
   reverbNode.time = 0; // seconds
   reverbNode.wet.value = 0.8;
   reverbNode.dry.value = 1;
   reverbNode.filterType = 'lowpass';
   reverbNode.cutoff.value = 4000; // Hz
 
-  document.getElementById('reverb').addEventListener('change', function () {
-    var currReverb = this.value;
-    reverbNode.time = currReverb;
-  });
-
-
-  // TONE
-  var toneNode = audioContext.createBiquadFilter();
-  toneNode.type = "highshelf";
-  toneNode.frequency.value = 2000;  // Hz
-  toneNode.gain.value = 50;
-
-  document.getElementById('tone').addEventListener('change', function () {
-    var currTone = this.value;
-    toneNode.frequency.value = currTone;
-  });
-
+  // Connect effects
   microphoneStream.connect(volumeNode);
   volumeNode.connect(gainNode);
   volumeNode.connect(reverbNode);
@@ -111,37 +84,32 @@ function startMicrophone(stream) {
   gainNode.connect(audioContext.destination);
 
 
+  // FFT
+  var scriptProcessorFFTNode = audioContext.createScriptProcessor(2048, 1, 1);
+  scriptProcessorFFTNode.connect(volumeNode);
 
-  // Complete mic stream setup
-  scriptProcessorNode = audioContext.createScriptProcessor(BUFF_SIZE, 1, 1);
-  scriptProcessorNode.onaudioprocess = processMicrophoneBuffer;
-
-  microphoneStream.connect(scriptProcessorNode);
-
-
-
-
-  // Setup FFT
-  scriptProcessorFFTNode = audioContext.createScriptProcessor(2048, 1, 1);
-  scriptProcessorFFTNode.connect(gainNode);
-
-  analyserNode = audioContext.createAnalyser();
+  var analyserNode = audioContext.createAnalyser();
   analyserNode.smoothingTimeConstant = 0;
   analyserNode.fftSize = 2048;
 
   microphoneStream.connect(analyserNode);
-
   analyserNode.connect(scriptProcessorFFTNode);
 
   scriptProcessorFFTNode.onaudioprocess = function () {
-
-    // get the average for the first channel
+    // Get average from channel
     var array = new Uint8Array(analyserNode.frequencyBinCount);
     analyserNode.getByteFrequencyData(array);
 
-    // draw the spectrogram
+    // Draw spectrogram
     if (microphoneStream.playbackState == microphoneStream.PLAYING_STATE) {
-      draw( array, -1, 'freqDomainCanvas' );
+      draw(array, -1, 'freqDomainCanvas');
     }
   };
+}
+
+function getAudioCtx() {
+  if ( window.AudioContext ) return new window.AudioContext();
+  else if ( window.webkitAudioContext ) return new window.webkitAudioContext();
+  else if ( window.audioContext ) return new window.audioContext();
+  else alert( 'Browser audio requirements not met' );
 }
