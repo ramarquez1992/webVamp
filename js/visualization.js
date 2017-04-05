5fdc3aaa4d8f16bde1e8f19fe3d1387a2721db38
Richard Marquez <richard92m@me.com>
2017-04-05 00:55:19 -0500

    reformatted for common style

--- js/visualization.js
+++ js/visualization.js
@@ -1,31 +1,31 @@
-function draw( plotData, deltaY, elementName ) {
-	let newData = [];
+function draw(plotData, deltaY, elementName) {
+  let newData = [];
 
-	for ( let i = 0; i < plotData.length - 1; i++ ) {
-		if ( Math.abs( plotData[ i + 1 ] - plotData[ i ] ) >= deltaY ) {
-      newData.push( plotData[ i + 1 ]);
-		}
-	}
+  for (let i = 0; i < plotData.length - 1; i++) {
+    if (Math.abs(plotData[i + 1] - plotData[i]) >= deltaY) {
+      newData.push(plotData[i + 1]);
+    }
+  }
 
-	const canvas = document.getElementById( elementName );
-	let ctx = canvas.getContext( '2d' );
+  const canvas = document.getElementById(elementName);
+  let ctx = canvas.getContext('2d');
   ctx.lineWidth = 0.5;
-  ctx.strokeStyle="#000";
-	const height = canvas.height;
-	const width = canvas.width;
+  ctx.strokeStyle = "#000";
+  const height = canvas.height;
+  const width = canvas.width;
 
-	ctx.clearRect( 0, 0, width, height ); // clear canvas
-	ctx.beginPath();
-	ctx.moveTo( 0, height / 2 - newData[ 0 ] * height / 2 );
+  ctx.clearRect(0, 0, width, height); // clear canvas
+  ctx.beginPath();
+  ctx.moveTo(0, height / 2 - newData[0] * height / 2);
 
-	let x = 0;
-	let y = 0;
-	for ( let j in newData ) {
-		y = newData[ j ];
-		ctx.lineTo( x, height / 2 - y * height / 2 );
-		x += width / ( newData.length - 1.0 );
-	}
+  let x = 0;
+  let y = 0;
+  for (let j in newData) {
+    y = newData[j];
+    ctx.lineTo(x, height / 2 - y * height / 2);
+    x += width / ( newData.length - 1.0 );
+  }
 
-	ctx.stroke();
-	ctx.closePath();
+  ctx.stroke();
+  ctx.closePath();
 }
